8d03a33883f0b70d1a87fc16c31ac9a4f85dba57
Sagar Jadhav <sagar.jadhav@rtcamp.com>
2015-05-16 13:31:25 +0530

    Update Js path

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'marvy_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function marvy_customize_preview_js() {
-	wp_enqueue_script( 'marvy_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'marvy_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'marvy_customize_preview_js' );
