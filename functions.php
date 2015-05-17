6390a2f18b30c4cf3fc3b7d329916341ef735a6a
Sagar Jadhav <sagar.jadhav@rtcamp.com>
2015-05-17 16:40:12 +0530

    Added icon font style.

--- functions.php
+++ functions.php
@@ -1,75 +1,78 @@
 <?php
+
 /**
  * Marvy functions and definitions
  *
  * @package Marvy
  */
-
+/* Customizer */
 require get_template_directory() . '/customizer/customizer-framework.php';
+require get_template_directory() . '/customizer/marvy-functions.php';
 
 if ( ! function_exists( 'marvy_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function marvy_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Marvy, use a find and replace
-	 * to change 'marvy' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'marvy', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	function marvy_setup() {
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'marvy' ),
-	) );
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Marvy, use a find and replace
+		 * to change 'marvy' to the name of your theme in all the template files
+		 */
+		load_theme_textdomain( 'marvy', get_template_directory() . '/languages' );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
-	) );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'marvy_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+		 */
+		//add_theme_support( 'post-thumbnails' );
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary Menu', 'marvy' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+		) );
+
+		/*
+		 * Enable support for Post Formats.
+		 * See http://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support( 'post-formats', array(
+			'aside', 'image', 'video', 'quote', 'link',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'marvy_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+	}
+
 endif; // marvy_setup
 add_action( 'after_setup_theme', 'marvy_setup' );
 
@@ -81,8 +84,9 @@ add_action( 'after_setup_theme', 'marvy_setup' );
  * @global int $content_width
  */
 function marvy_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'marvy_content_width', 640 );
+	$GLOBALS[ 'content_width' ] = apply_filters( 'marvy_content_width', 640 );
 }
+
 add_action( 'after_setup_theme', 'marvy_content_width', 0 );
 
 /**
@@ -92,21 +96,27 @@ add_action( 'after_setup_theme', 'marvy_content_width', 0 );
  */
 function marvy_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'marvy' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
+		'name' => esc_html__( 'Sidebar', 'marvy' ),
+		'id' => 'sidebar-1',
+		'description' => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'after_widget' => '</aside>',
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
 	) );
 }
+
 add_action( 'widgets_init', 'marvy_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
 function marvy_scripts() {
+	//wp_deregister_style( 'open-sans' );
+	//wp_register_style( 'open-sans', false );
+
+	wp_enqueue_style( 'marvy-google-font', '//fonts.googleapis.com/css?family=Roboto:100,500,300,400' );
+	wp_enqueue_style( 'marvy-icon-font', get_template_directory_uri() . '/assets/font/themify-icons.css' );
 	wp_enqueue_style( 'marvy-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'marvy-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
@@ -117,6 +127,7 @@ function marvy_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
+
 add_action( 'wp_enqueue_scripts', 'marvy_scripts' );
 
 /**
