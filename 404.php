973d9dbf110e6c0fb669451c7898ff710aee94b5
Sagar <sagarjadhav27@gmail.com>
2016-06-11 21:48:20 +0530

    404 page

--- 404.php
+++ 404.php
@@ -7,50 +7,26 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+<div class="grid entry-grid">
+
+	<div id="primary" class="content-area grid-cell">
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'marvy' ); ?></h1>
+					<div class="not-found-tag"><?php esc_html_e( '404', 'marvy' ); ?></div>
+					<h1 class="page-title"><?php esc_html_e( 'The page your are looking for is not avaiable. Please search another', 'marvy' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'marvy' ); ?></p>
-
+				<div class="page-content entry-content">
 					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( marvy_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'marvy' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'marvy' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
 				</div><!-- .page-content -->
+				
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+</div>
 
 <?php get_footer(); ?>
