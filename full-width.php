31d22f1abbae2c664e0d099f9fbdd22308794613
Sagar <sagarjadhav27@gmail.com>
2016-11-13 21:48:58 +0530

    Full width template.

---
+++ full-width.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ * @package Marvy
+ */
+get_header();
+?>
+
+<div class="grid entry-grid">
+
+	<div id="primary" class="content-area grid-cell">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+				?>
+
+			<?php endwhile; // end of the loop.  ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div>
+
+<?php
+// Footer
+get_footer();
